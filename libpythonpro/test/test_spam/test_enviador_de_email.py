f18e64e3ff73df40a058893a73c79a41c05efb42
romilsoncordeiro <romilsonlemes@gmail.com>
2020-12-02 01:41:14 -0300

    Foi acidionado o Teste de Exceções
    close #46

--- libpythonpro/test/test_spam/test_enviador_de_email.py
+++ libpythonpro/test/test_spam/test_enviador_de_email.py
@@ -1,11 +1,15 @@
 import pytest
 
-from libpythonpro.spam.enviador_de_email import Enviador
+from libpythonpro.spam.enviador_de_email import Enviador, EmailEnvalido
 
 
 def test_criar_enviador_de_email():
     enviador = Enviador()
     assert enviador is not None
+
+
+"""TESTAR EMAIL VÁLIDOS
+========================"""
 
 
 @pytest.mark.parametrize(
@@ -21,3 +25,22 @@ def test_remetente(destinatario):
         'Primeira turma em homenagem a Jessica Ferrari.'
     )
     assert destinatario in resultado
+
+
+""" TESTAR EMAIL INVÁLIDOS
+========================"""
+
+
+@pytest.mark.parametrize(
+    'destinatario',
+    ['', 'romilsonlemes']
+)
+def test_remetente_invalido(destinatario):
+    enviador = Enviador()
+    with pytest.raises(EmailEnvalido):
+        enviador.enviar(
+            destinatario,
+            'romilsonlemes@gmail.com',
+            'Cursos da Python Pro',
+            'Primeira turma em homenagem a Jessica Ferrari.'
+        )
