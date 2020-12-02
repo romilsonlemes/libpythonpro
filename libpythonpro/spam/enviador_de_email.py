4d38a07bf25a0fcf9e20dd6698efd2c2e4a65cf3
romilsoncordeiro <romilsonlemes@gmail.com>
2020-12-02 01:41:14 -0300

    Foi acidionado o Teste de Exceções
    close #46

--- libpythonpro/spam/enviador_de_email.py
+++ libpythonpro/spam/enviador_de_email.py
@@ -1,3 +1,10 @@
 class Enviador:
     def enviar(self, remetente, destinatario, assunto, corpo):
+        if '@' not in remetente:
+            raise EmailEnvalido(f'Email de remetente inválido: {remetente}')
         return remetente
+
+
+class EmailEnvalido(Exception):
+    print('Foi Encontrato um email invalido ao tentar enviar')
+    pass
